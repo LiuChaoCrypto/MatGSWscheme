9129a8bd51246f3dd36c1eb6ee3115a0df473d52
LiuChaoCrypto <1071431263@qq.com>
2020-12-21 22:50:30 -0800

    Change MatGSWmain

--- MatGSWmain.cpp
+++ MatGSWmain.cpp
@@ -16,12 +16,10 @@
 using namespace std;
 
 int main() {
-
-	
-
-	//TestSecurity60_uint16(MatMulMat);
-	
+    //Testing the correctness of the bootstrapping procedure.
 	TestBootstrap_uint32();
+    //Computeing the time complexity of HAOscheme and MatGSWscheme.
+    //The first input (MatGSWscheme/HAOscheme) is to make clear the type of the scheme. The second input (128/192/256) is to make clear the params. 
 	TestSecurity_uint32(MatGSWscheme,256);
 	return 0;
 
