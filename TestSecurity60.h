00591d86565ae9feac8b6d6105696c9a11b4f7b6
LiuChaoCrypto <1071431263@qq.com>
2020-12-22 18:40:44 -0800

    Add description for TestSecurity60

--- TestSecurity60.h
+++ TestSecurity60.h
@@ -3,11 +3,11 @@
 #define TESTSECURITY60
 
 enum Method{HAOscheme, MatGSWscheme};
-void TestSecurity60_uint16(Method);
+void TestSecurity60_uint16(Method);//60 bit security, Q=2^16
 
-void TestSecurity128_uint16(Method);
+void TestSecurity128_uint16(Method);//128 bit security, Q=2^16
 
 
 
-void TestSecurity_uint32(Method,int security);
+void TestSecurity_uint32(Method,int security);//Q=2^32
 #endif // !TESTSECURITY60
